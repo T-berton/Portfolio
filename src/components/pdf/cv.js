bdb897a0a5f33b4d4e1f6c9b96d65bd4cecada04
T-berton <tberton@etud.insa-toulouse.fr>
2022-01-04 18:24:31 +0100

    pdf_change

---
+++ src/components/pdf/cv.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import SinglePagePDFViewer from "single-page";
+
+/* This is required only if the project file is located 
+inside the app. Otherwise you can use the external link of the pdf file*/
+import samplePDF from "CV_Berton.pdf";
+
+
+function cv() {
+    return (
+    <div className="App">
+      <h4>Single Page</h4>
+      <SinglePagePDFViewer pdf={samplePDF} />
+
+      <hr />
+
+      <hr />
+    </div>
+  );
+}
+
+export default cv;
